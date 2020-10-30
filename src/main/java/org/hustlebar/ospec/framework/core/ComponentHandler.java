53ce4e6ab73de2e7f27beac119da838eb857a107
Tham <Thamizharasu.Soundrapandian@softwareag.com>
2020-10-30 09:53:23 +0530

    Component code work started

--- src/main/java/org/hustlebar/ospec/framework/core/ComponentHandler.java
+++ src/main/java/org/hustlebar/ospec/framework/core/ComponentHandler.java
@@ -1,44 +1,52 @@
 package org.hustlebar.ospec.framework.core;
 
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.examples.Example;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.responses.ApiResponse;
 import org.hustlebar.ospec.framework.model.OComponent;
-import org.hustlebar.ospec.framework.model.OSchema;
 import org.hustlebar.ospec.framework.model.Openapi;
 
 import javax.enterprise.context.ApplicationScoped;
-import java.util.List;
+import javax.inject.Inject;
+import java.util.Map;
 
 @ApplicationScoped
 public class ComponentHandler {
+    @Inject
+    SchemaHandler schemaHandler;
+
     public Components handle(Openapi openapi) {
         return _handle(openapi);
     }
 
     private Components _handle(Openapi openapi) {
-        OComponent component = openapi.component();
-        if (component == null) {
+        OComponent oComponent = openapi.component();
+        if (oComponent == null) {
             return null;
         }
 
-        //handle schemas
+        return new Components()
+            .schemas(getSchemas(oComponent))
+            .examples(getExamples(oComponent))
+            .responses(getResponses(oComponent))
+            .parameters(getParameters(oComponent));
+    }
 
-        //handle examples
-
-        //handle responses
-
-        //handle parameters
-
+    private Map<String, Parameter> getParameters(OComponent oComponent) {
         return null;
     }
 
-    private void _handleSchemas(OComponent component) {
-        List<OSchema> schemas = component.schemas();
-        if (schemas == null || schemas.isEmpty()) {
-            return;
-        }
+    private Map<String, ApiResponse> getResponses(OComponent oComponent) {
+        return null;
+    }
 
-        for (OSchema schema: schemas) {
+    private Map<String, Example> getExamples(OComponent oComponent) {
+        return null;
+    }
 
-        }
+    private Map<String, Schema> getSchemas(OComponent component) {
+        return schemaHandler.handle(component);
     }
 }
--- src/main/java/org/hustlebar/ospec/framework/core/SchemaHandler.java
+++ src/main/java/org/hustlebar/ospec/framework/core/SchemaHandler.java
@@ -1,4 +1,12 @@
 package org.hustlebar.ospec.framework.core;
 
+import io.swagger.v3.oas.models.media.Schema;
+import org.hustlebar.ospec.framework.model.OComponent;
+
+import java.util.Map;
+
 public class SchemaHandler {
+    public Map<String, Schema> handle(OComponent oComponent) {
+        return null;
+    }
 }
