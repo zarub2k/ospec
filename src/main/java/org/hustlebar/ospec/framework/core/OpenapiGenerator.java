56d071ac0eb4efd2488a76fd461a349fba276b31
Tham <zarub2k@gmail.com>
2020-11-03 11:46:54 +0530

    Small code change is done

--- src/main/java/org/hustlebar/ospec/framework/core/OpenapiGenerator.java
+++ src/main/java/org/hustlebar/ospec/framework/core/OpenapiGenerator.java
@@ -13,4 +13,22 @@ public class OpenapiGenerator {
     public OpenAPI generate(Openapi openapi) {
         return openapiParser.parse(openapi);
     }
+
+//    public OpenAPI get(Class<T> clazz) {
+//        Openapi openapi = null;
+//        try {
+//            OSpecification specification = clazz.newInstance();
+//            openapi = new Openapi()
+//                    .info(specification.getInfo())
+//                    .servers(specification.getServers())
+//                    .paths(specification.getPaths())
+//                    .component(specification.getComponent());
+//        } catch (InstantiationException e) {
+//            e.printStackTrace();
+//        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return this.generate(openapi);
+//    }
 }
